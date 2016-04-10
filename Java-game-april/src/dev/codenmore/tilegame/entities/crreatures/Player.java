1c0728c85d13104a6d180598c92570286f24a664
shishi2k15 <shivoskate@gmail.com>
2016-04-10 19:53:54 +0300

    add animations

--- Java-game-april/src/dev/codenmore/tilegame/entities/crreatures/Player.java
+++ Java-game-april/src/dev/codenmore/tilegame/entities/crreatures/Player.java
@@ -2,11 +2,16 @@ package dev.codenmore.tilegame.entities.crreatures;
 
 import dev.codenmore.tilegame.Game;
 import dev.codenmore.tilegame.Handler;
+import dev.codenmore.tilegame.gfx.Animation;
 import dev.codenmore.tilegame.gfx.Assets;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public  class Player extends Creature {
+
+    //Animation
+    private Animation animDown, animUp, animRight, animLeft;
 
     public Player(Handler handler, float x, float y) {
         super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
@@ -15,10 +20,23 @@ public  class Player extends Creature {
         bounds.y = 12;
         bounds.width = 20;
         bounds.height = 18;
+
+        //Animation
+        animDown = new Animation(100, Assets.player_down);
+        animUp = new Animation(100, Assets.player_up);
+        animRight = new Animation(100, Assets.player_right);
+        animLeft = new Animation(100, Assets.player_left);
     }
 
     @Override
     public void tick() {
+        //Animaitons
+        animDown.tick();
+        animRight.tick();
+        animLeft.tick();
+        animUp.tick();
+
+        //Move
         getInput();
         move();
         handler.getGameCamera().centerOnEntity(this);
@@ -44,10 +62,22 @@ public  class Player extends Creature {
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player_down[0], (int)(x - handler.getGameCamera().getxOffset()),
+        g.drawImage(getCurrentAnimationFrame(), (int)(x - handler.getGameCamera().getxOffset()),
                                         (int)(y - handler.getGameCamera().getyOffset()), width, height, null);
 //        g.setColor(Color.red); //// debug
 //        g.fillRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()),
 //                (int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
+    }
+
+    private BufferedImage getCurrentAnimationFrame(){
+        if (xMove < 0){
+            return animLeft.getCurrentFrame();
+        } else if (xMove > 0){
+            return animRight.getCurrentFrame();
+        } else if (yMove < 0){
+            return animUp.getCurrentFrame();
+        } else {
+            return animDown.getCurrentFrame();
+        }
     }
 }
--- Java-game-april/src/dev/codenmore/tilegame/gfx/Assets.java
+++ Java-game-april/src/dev/codenmore/tilegame/gfx/Assets.java
@@ -8,38 +8,28 @@ public class Assets {
 
     private static final int width = 24, height = 26;
 
-    public static BufferedImage grass, tree, stone, dirt;
-    public static BufferedImage[] player_down, player_up, player_left, player_right;
+    public static BufferedImage  grass, tree, stone, dirt;
+    public static BufferedImage[]  player_up, player_down, player_left, player_right;
 
     public static void init(){
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
-        SpriteSheet animationSheet = new SpriteSheet(ImageLoader.loadImage("/textures/playerAnimationSheet.png"));
-
         //player
         player_down = new BufferedImage[2];
         player_up = new BufferedImage[2];
-        player_left = new BufferedImage[2];
         player_right = new BufferedImage[2];
+        player_left = new BufferedImage[2];
 
-        player_down[0] = animationSheet.crop(width * 4, 0, width, height);
-        player_down[1] = animationSheet.crop(width * 5, 0, width, height);
+        player_down[0] = sheet.crop(width * 4, 0, width, height);
+        player_down[1] = sheet.crop(width * 5, 0, width, height);
 
-        player_up[0] = animationSheet.crop(width * 6, 0, width, height);
-        player_up[1] = animationSheet.crop(width * 7, 0, width, height);
+        player_up[0] = sheet.crop(width * 6, 0, width, height);
+        player_up[1] = sheet.crop(width * 7, 0, width, height);
 
-        player_left[0] = animationSheet.crop(width * 4, 0, width, height);
-        player_left[1] = animationSheet.crop(width * 5, 0, width, height);
+        player_right[0] = sheet.crop(width * 4, height, width, height);
+        player_right[1] = sheet.crop(width * 5, height, width, height);
 
-        player_right[0] = animationSheet.crop(width * 6, 0, width, height);
-        player_right[1] = animationSheet.crop(width * 7, 0, width, height);
-
-
-
-
-
-
-
-
+        player_left[0] = sheet.crop(width * 6, height, width, height);
+        player_left[1] = sheet.crop(width * 7, height, width, height);
 
 
         //world
